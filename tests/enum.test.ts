5d2444a74de18cd6935aa8ae006ea50f15df7870
Husnul <husnulf936@gmail.com>
2025-11-22 14:36:41 +0700

    test: add TypeScript examples for object types, optional properties, enums, and null/undefined

---
+++ tests/enum.test.ts
@@ -0,0 +1,12 @@
+import { CustomerType, type Customer } from "../src/enum";
+
+describe("Enum", function () {
+  it("Should support basic enum functionality", function () {
+    const customer: Customer = {
+      id: 1,
+      name: "Husnul",
+      type: CustomerType.GOLD
+    };
+    console.info(customer);
+  });
+});
---
+++ tests/optional.test.ts
@@ -0,0 +1,15 @@
+describe("Optional Paramater", function () {
+  it("Should support optional parameter in interface", function () {
+    function sayHello(name?: string | null) {
+      if (name) {
+        console.info(`Hello, ${name}`);
+      } else {
+        console.info("Hello");
+      }
+    }
+    sayHello("Husnul");
+    const name: string | undefined = undefined;
+    sayHello(name);
+    sayHello(null);
+  });
+});
