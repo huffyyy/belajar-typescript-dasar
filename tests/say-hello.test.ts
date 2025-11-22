71e96fed0583689a8126fdeda8d11d4fda6fa6c8
Husnul <husnulf936@gmail.com>
2025-11-22 10:39:48 +0700

    first Initial

---
+++ tests/say-hello.test.ts
@@ -0,0 +1,7 @@
+import { sayHello } from "../src/say-hello";
+
+describe("SayHello", function (): void {
+  it("should return hello husnul", function (): void {
+    expect(sayHello("Husnul")).toBe("Hello, Husnul");
+  });
+});
